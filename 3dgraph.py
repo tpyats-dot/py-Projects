4512ed40dabec50fd7e3be7b39b7f930a73b590e
tpyats-dot <tpyats@gmail.com>
2025-11-16 20:27:23 -0600

    Update 3dgraph.py

--- 3dgraph.py
+++ 3dgraph.py
@@ -6,7 +6,6 @@ HEIGHT = 30
 X_RANGE = (-10, 10)
 Y_RANGE = (-10, 10)
 
-# Ask the user for a function
 func_input = input("Enter a function f(x, y): ")
 
 def f(x, y):
@@ -15,14 +14,12 @@ def f(x, y):
     except:
         return 0
 
-# Generate the 3D plot as ASCII
 for i in range(HEIGHT):
     y = Y_RANGE[1] - (i / HEIGHT) * (Y_RANGE[1] - Y_RANGE[0])
     line = ""
     for j in range(WIDTH):
         x = X_RANGE[0] + (j / WIDTH) * (X_RANGE[1] - X_RANGE[0])
         z = f(x, y)
-        # Convert z to ASCII char
         if z > 5:
             char = "^"
         elif z > 2:
@@ -37,3 +34,4 @@ for i in range(HEIGHT):
             char = "_"
         line += char
     print(line)
+
